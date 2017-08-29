7211c7252d059884cfcd0e0d36fcb5213b06bb8b
Steve Hu <stevehu@gmail.com>
2017-08-29 15:13:29 -0400

    created two servers

---
+++ server2/src/main/java/hello/Server2.java
@@ -0,0 +1,96 @@
+package hello;
+
+import io.undertow.Handlers;
+import io.undertow.Undertow;
+import io.undertow.UndertowOptions;
+import io.undertow.io.Receiver;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Methods;
+
+import javax.net.ssl.*;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.KeyStore;
+
+public class Server2 {
+    private static final char[] STORE_PASSWORD = "password".toCharArray();
+
+    public static void main(final String[] args) throws Exception {
+        String version = System.getProperty("java.version");
+        System.out.println("Java version " + version);
+        if(version.charAt(0) == '1' && Integer.parseInt(version.charAt(2) + "") < 8 ) {
+            System.out.println("This example requires Java 1.8 or later");
+            System.out.println("The HTTP2 spec requires certain cyphers that are not present in older JVM's");
+            System.out.println("See section 9.2.2 of the HTTP2 specification for details");
+            System.exit(1);
+        }
+        String bindAddress = System.getProperty("bind.address", "localhost");
+        SSLContext sslContext = createSSLContext(loadKeyStore("/server.keystore"), loadKeyStore("/server.truststore"));
+        Undertow server = Undertow.builder()
+                .setServerOption(UndertowOptions.ENABLE_HTTP2, true)
+                .addHttpListener(8001, bindAddress)
+                .addHttpsListener(8443, bindAddress, sslContext)
+                .setHandler(getHandler()).build();
+
+        server.start();
+    }
+
+    private static HttpHandler getHandler() {
+        return Handlers.routing()
+                .add(Methods.GET, "/get", exchange -> {
+                    exchange.getResponseSender().send("Hello World");
+                })
+                .add(Methods.POST, "/post", exchange -> exchange.getRequestReceiver().receiveFullString(new Receiver.FullStringCallback() {
+                    @Override
+                    public void handle(HttpServerExchange exchange, String message) {
+                        exchange.getResponseSender().send(message + " World");
+                    }
+                }));
+    }
+
+    private static KeyStore loadKeyStore(String name) throws Exception {
+        String storeLoc = System.getProperty(name);
+        final InputStream stream;
+        if(storeLoc == null) {
+            stream = Server2.class.getResourceAsStream(name);
+        } else {
+            stream = Files.newInputStream(Paths.get(storeLoc));
+        }
+
+        if(stream == null) {
+            throw new RuntimeException("Could not load keystore");
+        }
+        try(InputStream is = stream) {
+            KeyStore loadedKeystore = KeyStore.getInstance("JKS");
+            loadedKeystore.load(is, password(name));
+            return loadedKeystore;
+        }
+    }
+
+    static char[] password(String name) {
+        String pw = System.getProperty(name + ".password");
+        return pw != null ? pw.toCharArray() : STORE_PASSWORD;
+    }
+
+
+    private static SSLContext createSSLContext(final KeyStore keyStore, final KeyStore trustStore) throws Exception {
+        KeyManager[] keyManagers;
+        KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+        keyManagerFactory.init(keyStore, password("key"));
+        keyManagers = keyManagerFactory.getKeyManagers();
+
+        TrustManager[] trustManagers;
+        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+        trustManagerFactory.init(trustStore);
+        trustManagers = trustManagerFactory.getTrustManagers();
+
+        SSLContext sslContext;
+        sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(keyManagers, trustManagers, null);
+
+        return sslContext;
+    }
+
+}
